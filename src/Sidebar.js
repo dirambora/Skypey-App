301fd8c76db53d9b3127eadc135b48ca8d8f09c1
unknown <dirambora>
2022-09-21 06:14:50 +0300

    user contact list

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -2,6 +2,6 @@ import React from 'react';
 import "./Sidebar.css";
 const Sidebar = () => {
     return <aside className="Sidebar">Sidebar</aside>;
-
+    contacts.map(contact => <User />)
 };
 export default Sidebar;
