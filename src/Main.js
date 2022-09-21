b4418571abfcb51d20f5726c7dc85f7ef43cc478
unknown <dirambora>
2022-09-21 05:57:09 +0300

    mmain and sidebar

---
+++ src/Main.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import "./Main.css";
+const Main = () => {
+    return <main className="Main">Main Stuff</main>;
+};
+export default Main;
---
+++ src/Sidebar.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import "./Sidebar.css";
+const Sidebar = () => {
+    return <aside className="Sidebar">Sidebar</aside>;
+
+};
+export default Sidebar;
