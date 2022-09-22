71a7b8fe3a357fde1efbfa056bb27abbe6fe9939
dirambora <diram.bora@noti.co.ke>
2022-09-22 11:09:07 +0300

    sidebar with user list

--- src/User.js
+++ src/User.js
@@ -3,7 +3,13 @@ import "./User.css";
 const User = ({ user }) => {
   const { name, profile_pic, status } = user;
   return (
-
+    <div className="User">
+      <img src={profile_pic} alt={name} className="User__pic" />
+      <div className="User__details">
+        <p className="User__details-name">{name}</p>
+        <p className="User__details-status">{status}</p>
+      </div>
+    </div> 
   );
 };
 
