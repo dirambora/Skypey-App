c3a7173d2c366c78b450a6488da4234c19d5abe5
dirambora <diram.bora@noti.co.ke>
2022-10-03 18:27:57 +0300

    sidebarusers

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import ReactDOM from "react-dom";
 import React from "react";
 import "./index.css";
-import App from "../containers/App";
 import store from "./store";
 const render = () => {
   fancyLog();
