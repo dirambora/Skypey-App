9f5a3c98db8ffc28cb2f56eada1171ff557ac49f
unknown <dirambora>
2022-10-06 06:33:47 +0300

    Added chats and header componets

---
+++ src/Components/Chats.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import "./Chats.css";
+const Chat = ({ message }) => {
+    //ES6 destructuring syntax
+  const { text, is_user_msg } = message;
+  return (
+    <span className={`Chat ${is_user_msg ? "is-user-msg" : ""}`}
+      >{text}</span>
+  ); 
+};
+class Chats extends Component {
+  render() {
+    return (
+      <div className="Chats">
+        {this.props.messages.map(message => (
+          <Chat message={message} key={message.number} />
+        ))} </div>
+    ); 
+  }
+}
+export default Chats;
